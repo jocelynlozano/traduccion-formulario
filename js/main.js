bfdbc170bd4f391f0a30e0464b967f1606e33ca2
jocelynlozano <jocelynlozanocarbajal@gmail.com>
2016-09-14 13:58:29 -0500

    modificación de datos- ingles a español

---
+++ js/main.js
@@ -0,0 +1,5 @@
+document.getElementsByTagName("h2")[0].innerHTML="Porfavor inicia sesión";
+document.getElementById("inputEmail").placeholder="Correo Electrónico";
+document.getElementById("inputPassword").placeholder="Contraseña";
+document.querySelector("span").innerHTML="Recordar datos";
+document.querySelector("button").innerHTML="Iniciar Sesión";
